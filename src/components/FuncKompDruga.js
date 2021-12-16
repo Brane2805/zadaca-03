913b9111ccac27644723af9c37982ca8e60af5f5
Branimir Matijacic <bmatijacic@nsk.hr>
2021-12-16 08:58:39 +0100

    Komponenta definirana Funkcijom sa children propsima

--- src/components/FuncKompDruga.js
+++ src/components/FuncKompDruga.js
@@ -5,7 +5,7 @@ const FuncKompDruga = (props) => {
     return (
         <div>
         <h2> Komponenta definirana drugom funkcijom sa ispisom proslijeđenih child propsa:</h2>
-        {props.children}
+        {props.children} 
         </div>
     );
 }
